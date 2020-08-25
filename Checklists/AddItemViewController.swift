22034820fd0db01151987dfa90178d3fa15fe139
Vanessa Flores <floresv26@gmail.com>
2020-08-25 16:28:50 -0500

    Create Add Item screen with navigation

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Vanessa Flores on 8/25/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction private func done() {
+        navigationController?.popViewController(animated: true)
+    }
+}
