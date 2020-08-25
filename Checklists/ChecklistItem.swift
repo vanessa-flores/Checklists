e5392a9b2b3ff92d0cb4f52a3ced531b3d1bf8ac
Vanessa Flores <floresv26@gmail.com>
2020-08-25 14:02:38 -0500

    Add items using an array

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Vanessa Flores on 8/25/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text: String = ""
+    var checked: Bool = false
+    
+    func toggleChecked() {
+        checked.toggle()
+    }
+}
