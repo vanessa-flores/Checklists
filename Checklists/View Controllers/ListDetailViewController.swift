8e5aae904359e46adf48abaeef8c3648a82f9525
Vanessa Flores <floresv26@gmail.com>
2020-08-27 16:57:03 -0500

    Set up Add / Edit Checklist

---
+++ Checklists/View Controllers/ListDetailViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Vanessa Flores on 8/27/20.
+//  Copyright © 2020 Rising Dev Habits. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController)
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist)
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing: Checklist)
+}
+
+class ListDetailViewController: UITableViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction private func done() {
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditing: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAdding: checklist)
+        }
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        
+        return true
+    }
+}
