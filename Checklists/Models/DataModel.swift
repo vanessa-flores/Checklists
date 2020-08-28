211c9babaef297b6ec17a2d0f186e7d42f68a885
Vanessa Flores <floresv26@gmail.com>
2020-08-28 14:41:26 -0500

    Automatically create a list and navigate to the list when the app is used for the first time

--- Checklists/Models/DataModel.swift
+++ Checklists/Models/DataModel.swift
@@ -24,6 +24,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
             
     private func documentsDirectory() -> URL {
@@ -62,7 +63,21 @@ class DataModel {
     }
     
     private func registerDefaults() {
-        let dictionary = ["ChecklistIndex": -1]
+        let dictionary = ["ChecklistIndex": -1, "FirstTime": true] as [String : Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    private func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
 }
