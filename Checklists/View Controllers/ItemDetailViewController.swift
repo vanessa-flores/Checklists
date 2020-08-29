b5ada9cf5ddd22fc03d48dd80aea4015a58e8656
Vanessa Flores <floresv26@gmail.com>
2020-08-29 11:42:03 -0500

    Update reminder and due date values

--- Checklists/View Controllers/ItemDetailViewController.swift
+++ Checklists/View Controllers/ItemDetailViewController.swift
@@ -83,9 +83,17 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     @IBAction private func done() {
         if let item = itemToEdit {
             item.text = textField.text!
+            
+            item.shouldRemind = shouldRemindSwitch.isOn
+            item.dueDate = dueDate
+            
             delegate?.itemDetailViewController(self, didFinishEditing: item)
         } else {
             let item = ChecklistItem(text: textField.text!)
+            
+            item.shouldRemind = shouldRemindSwitch.isOn
+            item.dueDate = dueDate
+            
             delegate?.itemDetailViewController(self, didFinishAdding: item)
         }
     }
